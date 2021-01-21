d8c6fd5eca9ed9a323ce0bce4498a2826d5ddb73
RachelNS <rachelnelsonschille@gmail.com>
2021-01-21 10:00:11 -0800

    created add and remove employee files

---
+++ addEmp.js
@@ -0,0 +1,9 @@
+const mysql = require("mysql");
+const start = require("./index.js");
+const table = require("console.table");
+
+// You need to create an array of objects where the name key is the manager's name and the value key is the manager ID, and pass that to the inquirer choices
+
+
+
+module.exports = {allEmp};
---
+++ remEmp.js
@@ -0,0 +1,6 @@
+const mysql = require("mysql");
+const start = require("./index.js");
+const table = require("console.table");
+
+
+module.exports = {remEmp};
